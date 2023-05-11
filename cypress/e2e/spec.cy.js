dbc3e1b1eceba86b7d04e67e429a3c6fbafd0d3b
OlenaBeyond <olena.novgorodchenko@getbeyond.com>
2023-05-11 19:11:54 +0200

    mocha

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,76 @@
+
+
+describe("Negatve Test suit" , () => {
+it("Test 1", () => {
+  cy.visit('https://example.com');
+})
+
+it("Test 1", () => {
+  cy.visit('https://example.com');
+})
+
+it("Test 1", () => {
+  cy.visit('https://example.com');
+})
+
+
+it("Test 1", () => {
+  cy.visit('https://example.com');
+})
+})
+
+describe("Positive Test suit" , () => {
+  it.skip("Test 1", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 1", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 1", () => {
+    cy.visit('https://example.com');
+  })
+  
+  it("Test 1", () => {
+    cy.visit('https://example.com');
+  })
+  })
+
+  describe("Some  Test suit" , () => {
+    it("Test 1", () => {
+      cy.visit('https://example.com');
+    })
+    
+    it("Test 1", () => {
+      cy.visit('https://example.com');
+    })
+    
+    it("Test 1", () => {
+      cy.visit('https://example.com');
+    })
+    
+    
+    it("Test 1", () => {
+      cy.visit('https://example.com');
+    })
+
+    describe("Another Test suit" , () => {
+      it("Test 1", () => {
+        cy.visit('https://example.com');
+      })
+      
+      it("Test 1", () => {
+        cy.visit('https://example.com');
+      })
+      
+      it("Test 1", () => {
+        cy.visit('https://example.com');
+      })
+      
+      
+      it("Test 1", () => {
+        cy.visit('https://example.com');
+      })
+      })
+    })

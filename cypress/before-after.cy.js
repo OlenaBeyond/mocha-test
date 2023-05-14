db443f8b24e4ce85c63150e2b7eb7ae033d2e26a
OlenaBeyond <olena.novgorodchenko@getbeyond.com>
2023-05-14 10:11:02 +0200

    added 2 tests

---
+++ cypress/before-after.cy.js
@@ -0,0 +1,10 @@
+describe("Positive Test suit" , () => {
+
+    it("Test 1", () => {
+      cy.visit('https://example.com');
+    })
+    
+    it("Test 1", () => {
+      cy.visit('https://example.com');
+    })
+})
